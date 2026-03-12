b11596247b0d336866dd6807218eb6294643cef9
imsamaritan <aicodemate.talk@gmail.com>
2026-03-12 10:24:21 +0200

    Created Core/Router and added code for adding "add" routes into a route table and "match" method for matching incoming request route with application routes.

---
+++ src/Core/Router.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Core;
+
+class Router
+{
+  private array $routes = [];
+
+  public function add(string $path, array $params = []): void
+  {
+    $this->routes[] = ["path" => $path, "params" => $params];
+  }
+
+  public function match(string $url_path): array | false
+  {
+    $url_path = strtolower($url_path);
+    $url_path = rtrim($url_path);
+
+    foreach ($this->routes as $route) {
+      if ($route["path"] === $url_path) {
+        return $route["params"];
+      }
+    }
+
+    return false;
+  }
+}
