2fb6edf0a6e955f0beda1040b5dc7a6b8e029d20
imsamaritan <aicodemate.talk@gmail.com>
2026-03-12 11:32:08 +0200

    If url path match any app route
    - Get controller name
    - Get method name
    - Check if construct does not exists. If yes, then exit the script
    - Check if a given method does not exists inside the constructor. If
      yes, then exit the script
      -If all checked out, create a controller instance
    - Run a given method from a given constructor

--- index.php
+++ index.php
@@ -22,3 +22,26 @@ $matched_route = $router->match($url_path);
 if ($matched_route === false) {
   exit("Resource '{$url_path}', was not found!");
 }
+
+#Get controller name
+$namespace = "App\Controllers\\";
+$controller = $namespace . ucwords($matched_route["controller"]);
+
+#Get action name as a method
+$method = $matched_route["action"];
+
+#Check if a class does not exists, then exit if it true
+if (class_exists($controller) === false) {
+  exit("Constructor '{$controller}', is not defined or does not exists!");
+}
+
+#Check if a method exist inside the controller constructor
+if (method_exists($controller, $method) === false) {
+  exit("Method '{$method}', does not exists inside '{$controller}' constructor!");
+}
+
+#Instantiate constructor object instance 
+$controller_instance = new $controller;
+
+#Run a method from the instance 
+$controller_instance->$method();
