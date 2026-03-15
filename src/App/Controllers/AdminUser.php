2644748f226d8aaa9c532e12f02137461b2e2f41
imsamaritan <aicodemate.talk@gmail.com>
2026-03-15 19:13:11 +0000

    Implemented getControllerName in the dispatcher and used it inside the handle method.

---
+++ src/App/Controllers/AdminUser.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class AdminUser
+{
+  public function listAdmins(): void
+  {
+    echo "Listing all admins from the database";
+  }
+}
