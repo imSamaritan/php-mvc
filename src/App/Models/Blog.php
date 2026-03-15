29ca28451f8510834008d559bc1db2fef01df8cc
imsamaritan <aicodemate.talk@gmail.com>
2026-03-16 01:42:27 +0200

    Implemented find and findAll methods inside the blog for getting data

--- src/App/Models/Blog.php
+++ src/App/Models/Blog.php
@@ -2,5 +2,40 @@
 declare(strict_types=1);
 
 namespace App\Models;
+use App\Database;
 
-class Blog {}
+class Blog
+{
+  private Database $db;
+
+  public function __construct()
+  {
+    $this->db = Database::instance();
+  }
+
+  public function findAll(): array|false
+  {
+    $connection = $this->db->connect();
+    $sql = "SELECT * FROM blogs";
+    $statement = $connection->prepare($sql);
+
+    if ($statement->execute()) {
+      return $statement->fetchAll();
+    }
+
+    return false;
+  }
+  
+  public function find(int $id): object|false
+  {
+    $connection = $this->db->connect();
+    $sql = "SELECT * FROM blogs WHERE id = :id";
+    $statement = $connection->prepare($sql);
+
+    if ($statement->execute(["id" => $id])) {
+      return $statement->fetch();
+    }
+
+    return false;
+  }
+}
