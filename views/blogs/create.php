2ba8c949f3faadc1ddf2f36080c7e1de0fd32c3f
imsamaritan <aicodemate.talk@gmail.com>
2026-04-11 20:18:33 +0200

    Implemented blog update through the base model update method
    - Add update method in the base model
    - Handling update on base model through edit method
    - Added a form component in the project

--- views/blogs/create.php
+++ views/blogs/create.php
@@ -1,26 +1,4 @@
+<a href="/blogs">&lt;&lt;Go back</a>
 <form method="POST" action="/blogs/create">
-  <div class="field">
-    <label for="author">Author</label>
-    <input type="text" id="author" name="author"/>
-    <?php if (isset($error["author"])): ?>
-      <small><?php echo $error["author"] ?></small>
-    <?php endif ?>
-  </div>
-  <div class="field">
-    <label for="title">Title</label>
-    <input type="text" id="title" name="title"/>
-    <?php if (isset($error["title"])): ?>
-      <small><?php echo $error["title"] ?></small>
-    <?php endif ?>
-  </div>
-  <div class="field">
-    <label for="post">Post</label>
-    <textarea type="text" id="post" name="body"></textarea>
-    <?php if (isset($error["body"])): ?>
-      <small><?php echo $error["body"] ?></small>
-    <?php endif ?>
-  </div>  
-  <div class="field">
-    <button type="submit" name="submit">Post</button>
-  </div>
+  <?php require dirname(__DIR__) . "/components/form.php"; ?>
 </form>
